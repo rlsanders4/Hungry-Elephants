3e1f27fb1d01657719ee404baf1cc68a76096d35
YilaiYan <yanyilai201708@gmail.com>
2021-03-02 12:26:13 -0500

    changeModel

--- website/datalog/models.py
+++ website/datalog/models.py
@@ -1,22 +1,11 @@
 from django.db import models
 # Create your models here.
 
-class RFID_tag_number(models.Model):
+class FeedingData(models.Model):
     
-    name = models.CharField(max_length=10)
+    rfid_tag_number = models.CharField(max_length=10)
+    unix_time = models.CharField(max_length=10)
+    site_code = models.CharField(max_length=3)
+    antenna_number = models.CharField(max_length=2)
 
-    
-class Unix_time(models.Model):
-   
-    name = models.CharField(max_length=10)
-
-class Site_code(models.Model):
-   
-    name = models.CharField(max_length=3)
-
-
-
-class Antenna_number(models.Model):
-   
-    name = models.CharField(max_length=2)
     
--- website/datalog/views.py
+++ website/datalog/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import RFID_tag_number,  Unix_time ,Site_code, Antenna_number
 
 # Create your views here.
 def index(request):
-    context = {'name': 'Hungry Elephants Data Visualisation', }
-    return render(request, 'datalog/index.html', context)
+    
+    rfid_tag_number = RFID_tag_number.objects.all()
+    
+    unix_time = Unix_time.objects.all()
+   
+    site_code = Site_code.objects.all()
+   
+    antenna_number = Antenna_number.objects.all()
+    
+    return render(request, 'datalog/index.html', {'RFID_tag_number':rfid_tag_number, 'Unix_time':unix_time, 'Site_code':site_code, 'Antenna_number':antenna_number, })
