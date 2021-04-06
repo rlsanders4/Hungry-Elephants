89b3e5cbb37fc0d7766592d81ba5a61bfe3c272b
Shivam <patel.shivam.m@gmail.com>
2021-04-05 22:48:24 -0400

    all models are included in the the admin.py files for each module

--- website/elephants/admin.py
+++ website/elephants/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Elephant, Schedule, Preset
+from .models import Elephant, Schedule, Preset, PresetSchedule
 
 admin.site.register(Elephant)
 admin.site.register(Schedule)
 admin.site.register(Preset)
+admin.site.register(PresetSchedule)
