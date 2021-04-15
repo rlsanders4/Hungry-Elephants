625af1d50f6f91d3445d49ccf432c5e3c5f7f538
Shivam <patel.shivam.m@gmail.com>
2021-04-15 17:01:46 -0400

    comments for elephants folder

--- website/elephants/admin.py
+++ website/elephants/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import Elephant, Schedule, Preset, PresetSchedule
 
+#this just registers models so they will appear in the admin page
 admin.site.register(Elephant)
 admin.site.register(Schedule)
 admin.site.register(Preset)
